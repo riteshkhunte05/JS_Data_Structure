44410969db13b36ab2f14a54c46cfb81a006d2f3
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-05-11 22:07:17 +0530

    Added: JS Map Problems

---
+++ MapProblems/BirthMonth.js
@@ -0,0 +1,21 @@
+let birthMonthMap = new Map([
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+    [9, 0],
+    [10, 0],
+    [11, 0],
+    [12, 0]
+]);
+for (let i = 1; i <= 50; i++) {
+    let randomMonth = Math.floor(Math.random() * 12) + 1;
+    let count = birthMonthMap.get(randomMonth);
+    birthMonthMap.set(randomMonth, ++count);
+}
+// Prints map 
+console.log(birthMonthMap);
